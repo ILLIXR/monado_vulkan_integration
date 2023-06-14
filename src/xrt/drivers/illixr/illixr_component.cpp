d7860c82f1e65b5ae6cae42619c614fc9d5d768f
shg8 <me@shg.life>
2023-06-14 21:53:59 +0800

    Add interface functions for timewarp_vk

--- src/xrt/drivers/illixr/illixr_component.cpp
+++ src/xrt/drivers/illixr/illixr_component.cpp
@@ -10,12 +10,15 @@
 
 #include "xrt/xrt_device.h"
 
+#include <vulkan/vulkan.h>
+
 #include <iostream>
 #include "common/plugin.hpp"
 #include "common/phonebook.hpp"
 #include "common/switchboard.hpp"
 #include "common/data_format.hpp"
 #include "common/pose_prediction.hpp"
+#include "common/vk_util/render_pass.hpp"
 
 using namespace ILLIXR;
 
@@ -25,11 +28,17 @@ class illixr_plugin : public plugin
 public:
 	illixr_plugin(std::string name_, phonebook *pb_)
 	    : plugin{name_, pb_}, sb{pb->lookup_impl<switchboard>()}, sb_pose{pb->lookup_impl<pose_prediction>()}
+		, sb_timewarp{pb->lookup_impl<timewarp>()}
 	{}
 
 	const std::shared_ptr<switchboard> sb;
 	const std::shared_ptr<pose_prediction> sb_pose;
+	const std::shared_ptr<timewarp> sb_timewarp;
 };
+
+class monado_vulkan_display_sink : public display_sink {
+
+}
 
 static illixr_plugin *illixr_plugin_obj = nullptr;
 
@@ -62,4 +71,23 @@ illixr_read_pose()
 	ret.position.z = pose.position.z();
 
 	return ret;
+}
+
+extern "C" void *illixr_initialize_vulkan_display_service(VkInstance instance, VkPhysicalDevice physical_device, VkDevice device, VkQueue queue, uint32_t queue_family_index, VkExtent2D swapchain_extent) {
+	assert(illixr_plugin_obj && "illixr_plugin_obj must be initialized first.");
+	auto ds = std::make_shared<monado_vulkan_display_sink>();
+	ds->vk_instance = instance;
+	ds->vk_physical_device = physical_device;
+	ds->vk_device = device;
+	ds->graphics_queue = queue;
+	ds->graphics_queue_family = queue_family_index;
+	ds->swapchain_extent = swapchain_extent;
+}
+
+extern "C" void *illixr_initialize_timewarp(VkRenderPass render_pass, uint32_t subpass, VkImageView* buffer_pool, uint32_t num_buffers_per_eye) {
+	assert(illixr_plugin_obj && "illixr_plugin_obj must be initialized first.");
+	std::vector<VkImageView> left_eye_views(buffer_pool, buffer_pool + num_buffers_per_eye);
+	std::vector<VkImageView> right_eye_views(buffer_pool + num_buffers_per_eye, buffer_pool + 2 * num_buffers_per_eye);
+	std::array<std::vector<VkImageView>, 2> eye_views = {left_eye_views, right_eye_views};
+	illixr_plugin_obj->sb_timewarp->setup(render_pass, subpass, std::move(eye_views));
 }
--- src/xrt/drivers/illixr/illixr_component.h
+++ src/xrt/drivers/illixr/illixr_component.h
@@ -9,6 +9,8 @@
 
 #pragma once
 
+#include <vulkan/vulkan.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -17,6 +19,9 @@ void *
 illixr_monado_create_plugin(void *pb);
 struct xrt_pose
 illixr_read_pose();
+
+void *illixr_initialize_vulkan_display_service(VkInstance instance, VkPhysicalDevice physical_device, VkDevice device, VkQueue queue, uint32_t queue_family_index, VkExtent2D swapchain_extent);
+void *illixr_initialize_timewarp(VkRenderPass render_pass, uint32_t subpass, VkImageView* buffer_pool, uint32_t num_buffers);
 
 #ifdef __cplusplus
 }
